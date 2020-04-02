1b2c4b2b63d9f26c68bae42f6dc8b35f2a0ecdca
Gary <fishellgary@gmail.com>
2020-04-02 15:26:06 -0700

    Added credits, menu and splash screen

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour
+{
+    public void ReturnToMenu() {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+}
+
+
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+            Application.Quit();
+
+        #endif
+    }
+
+
+}
